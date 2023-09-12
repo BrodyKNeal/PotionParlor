3e8c7c3887cd4b98507ae64abc8a9001c0d093fc
Phil-Eggtree <icb0003@uah.edu>
2023-09-11 21:51:39 -0500

    Add the second version
    Added second version with drag and drop features

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D body;
+    public Animator animator;
+    Vector2 movement;
+
+    //Update called once per frame for physics
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized; //normalize the movement vector so diagonal movement isn't faster
+
+
+        //Prevents animator from viewing movement data if the player is not moving.
+        //This will cause the animator to keep the player in the same direction they were facing when they stop moving
+        if(movement.sqrMagnitude > 0)
+        {
+            animator.SetFloat("Horizontal", movement.x); //Sets the animator's variable called "Horizontal" to the player's input left/right
+            animator.SetFloat("Vertical", movement.y);
+        }
+
+        animator.SetFloat("Speed", movement.sqrMagnitude); //Sets the animator's variable called "Speed" to the magnitude of the player's movement
+    }
+
+    //The update method that's better for physics
+    void FixedUpdate()
+    {
+        body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
